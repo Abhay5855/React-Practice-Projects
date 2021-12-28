f8ec80e50e84df08716f9ea133973517fa9833fa
Abhay5855 <ap3919529@gmail.com>
2021-12-28 18:41:31 +0530

    creating a state and set the colors accoriding to the condition on  click

--- react practice projects practice/src/usestate/State.js
+++ react practice projects practice/src/usestate/State.js
@@ -1,19 +1,26 @@
-
+import { useState } from "react";
 
 const State = () => {
+  const [isGreen, setIsgreen] = useState(true);
 
+  const changeColor = () => {
+    setIsgreen(!isGreen);
+  };
 
-    return (
-
-         <>
-
-         <h2 style={{textAlign:'center' , cursor:'pointer'}}>I love using React JS</h2>
-
-         </>
-    )
-
-       
-}
-
+  return (
+    <>
+      <h2
+        onClick={() => changeColor()}
+        style={{
+          textAlign: "center",
+          cursor: "pointer",
+          color: isGreen ? "green" : "red",
+        }}
+      >
+        I love using React JS
+      </h2>
+    </>
+  );
+};
 
 export default State;
