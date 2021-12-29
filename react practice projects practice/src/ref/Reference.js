911db3d691b4a212a15639c1de3920e51d4c2a06
Abhay5855 <ap3919529@gmail.com>
2021-12-29 15:51:32 +0530

    creating two reference on the inputs and two buttons

--- react practice projects practice/src/ref/Reference.js
+++ react practice projects practice/src/ref/Reference.js
@@ -1,13 +1,38 @@
+import { useRef, useState } from "react";
 
 
 
 
 const Reference = () => {
 
+         const [val1 , setVal1] = useState(0);
+
+         const [val2 , setVal2] = useState('');
+
+         const inputOne = useRef();
+
+         const inputTwo = useRef();
+           
+
+         console.log(inputOne.current);
+
+
          return (
              <>
 
+
+             
              <h2>Use ref hook</h2>
+
+             <input type='number'  ref={inputOne} value={val1}/>
+
+             <br/>
+             <input type='text'  ref={inputTwo} value={val2}/>
+
+             <br />
+
+             <button onClick={() => handleOne()}>one</button>
+             <button onClick={() => handleTwo()}>two</button>
              </>
          )
 }
