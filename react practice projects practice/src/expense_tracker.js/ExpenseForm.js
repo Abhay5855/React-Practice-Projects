5727b792dfc3d19b3e3ed7eefd8058cb8399774d
Abhay5855 <ap3919529@gmail.com>
2022-01-05 22:14:28 +0530

    created input for charge and amount and submit button to submit the values ibside the form

--- react practice projects practice/src/expense_tracker.js/ExpenseForm.js
+++ react practice projects practice/src/expense_tracker.js/ExpenseForm.js
@@ -1,18 +1,62 @@
-
-
-
-
-
-
+import ExpenseContainer from "./ExpenseContainer";
+import { useState } from "react";
 
 const ExpenseForm = () => {
+  const [charge, setCharge] = useState('');
+
+  const [amount, setAmount] = useState();
 
 
-    return (
+//handle form
 
-     <></>
-    )
-}
+   const handleSubmit = (e) => {
+    e.preventDefault(); 
 
+
+    if(amount > 0 && charge.trim()){
+
+         console.log('amount added ');
+         console.log(charge + '-> ' + amount);
+
+    }
+
+    setAmount('');
+    setCharge('');
+               
+   }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <div className="">
+            <label for="charge">Charge</label>
+            <input
+              type="text"
+              value={charge}
+              onChange={(e) => setCharge(e.target.value)}
+              placeholder="e.g rent"
+            />
+            <br />
+
+            <label for="charge">Amount</label>
+
+            <input
+              type="text"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              placeholder="e.g 100"
+            />
+
+            <br/>
+
+             <button type="submit">submit</button>
+              
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default ExpenseForm;
