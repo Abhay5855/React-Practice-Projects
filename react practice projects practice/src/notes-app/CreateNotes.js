73d9a404b72c30a4a92e0a1939703a757033ca69
Abhay5855 <ap3919529@gmail.com>
2022-01-07 01:04:48 +0530

    creating input feilds and button to submit the note

--- react practice projects practice/src/notes-app/CreateNotes.js
+++ react practice projects practice/src/notes-app/CreateNotes.js
@@ -1,30 +1,37 @@
 import { useState } from "react/cjs/react.development";
-
-
-
+import './notes.css'
 const CreateNotes = () => {
+  const [heading, setHeading] = useState("");
+  const [description, setDescription] = useState("");
 
+  return (
+    <>
+    
+      <div className="container__notes">
+        <h3>Create Notes</h3>
 
-    const [heading , setHeading] = useState('');
-    const [description , setDescription] = useState('');
+         <form required>
 
+             <div className="create__notes">
 
+                  <input type='text' placeholder='Enter Title..'  />
 
+                  <span>Add description</span>
 
-    return (
-        <>
+                <textarea placeholder="Add description.."></textarea>
 
-        <div className="container__notes">
+                <button>
 
-            <h3>Add Notes</h3>
+                <i class="fas fa-plus-square"></i>
 
-              
-        </div>
-        </>
-    )
+                </button>
 
-         
-}
+             </div>
 
+         </form>
+      </div>
+    </>
+  );
+};
 
 export default CreateNotes;
